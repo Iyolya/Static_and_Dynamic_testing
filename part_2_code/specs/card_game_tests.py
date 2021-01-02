43ed16001e82d2435ddc2421216a2de54c26e30d
Lyolya <almasilaura@outlook.com>
2021-01-02 11:48:19 +0000

    TEST of 'check_for_ace' PASSED

--- part_2_code/specs/card_game_tests.py
+++ part_2_code/specs/card_game_tests.py
@@ -6,5 +6,9 @@ class TestCardGame(unittest.TestCase):
     def setUp(self):
         self.card1 = Card("Hearts", 4)
         self.card2 = Card("Clubs", 8)
+        self.cards = [self.card1, self.card2]
+        self.card_game = CardGame()
        
 
+    def test_check_ace(self):
+        self.assertEqual(False, self.card_game.check_for_ace(self.card1))
